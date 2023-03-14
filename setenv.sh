5850ef9be8d42a2fa18bbb04ec77ff3ff867a5e5
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2023-03-14 10:12:34 -0400

    add setenv updates

--- setenv.sh
+++ setenv.sh
@@ -3,9 +3,21 @@
 LARMAE_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo ${LARMAE_HOME}
 
-cd /home/twongjirad/working/larbys/icarus/icdl
-source setenv_py3.sh
-source configure.sh
+MACHINE=`uname --nodename`
 
+if [ $MACHINE == "pop-os" ]
+then
+
+    cd /home/twongjirad/working/larbys/icarus/icdl
+    source setenv_py3.sh
+    source configure.sh
+
+else
+    echo "DEFAULT SETUP (COMPAT WITH SINGULARITY CONTAINER)"    
+fi
+    
 echo ${LARMAE_HOME}
 cd ${LARMAE_HOME}
+
+# ADD VIT-PYTORCH FOLDER INTO PYTHON PATH
+[[ ":$PYTHONPATH:" != *":${LARMAE_HOME}/vit-pytorch:"* ]] && export PYTHONPATH="${LARMAE_HOME}/vit-pytorch:${PYTHONPATH}"
