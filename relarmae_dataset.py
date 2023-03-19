af1abbe257c0f9cf2b8ca045fd09341f1f0ce2e3
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2023-03-19 14:29:53 -0400

    another dataloader rewrite

---
+++ relarmae_dataset.py
@@ -0,0 +1,273 @@
+import os,time,copy,sys
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import ROOT as rt
+from larcv import larcv
+larcv.load_pyutil
+from ctypes import c_int
+import yaml
+
+from einops.layers.torch import Rearrange
+
+class relarmaeDataset(torch.utils.data.Dataset):
+    def __init__(self, cfg_file, seed=0 ):
+        """
+        Parameters:
+        
+        """
+        
+        with open(cfg_file, "r") as stream:
+            try:
+                allcfg = yaml.safe_load(stream)
+                print(allcfg)
+            except yaml.YAMLError as exc:
+                print(exc)
+                
+        self.cfg = allcfg.get('larmaeDataset')                
+        filelist = self.cfg.get('filelist',None)
+        
+        # every value is a numpy array. will this avoid reference counting problem?
+        self.cropsize = np.array( self.cfg.get("crop_size",512), dtype=np.int32 )
+        self.adc_threshold = np.array( self.cfg.get("adc_threshold",10.0) )
+        self.min_crop_pixels = np.array( self.cfg.get("min_crop_pixels",1000) )
+        self.patch_dims = self.cfg.get("patch_dims",16)
+        self.vector_index = np.array( self.cfg.get("vector_index",0), dtype=np.int32)
+        self.nonzero_patch_threshold = np.array( self.cfg.get("nonzero_patch_threshold",10) ) # our of 1024 patches for 512x512 image
+        self.get_batch = True
+        self.batch_size = self.cfg.get("batch_size",4)
+        self.rng = np.random.RandomState(seed)
+        self.offset = np.zeros(1)
+
+        # create ROOT TTree
+        self.tree = rt.TChain("extbnb_images")
+        
+        if filelist is not None:
+            for ifile in filelist:
+                self.tree.Add( ifile )
+
+        self.nentries = np.array( self.tree.GetEntries(), dtype=np.long )
+        print("larmaeDataset created. TChain=",self.tree)
+
+    def randomize_offset(self):
+        self.offset[0] = self.rng.randint(0,self.nentries,size=1)
+
+        
+    def __getitem__(self, idx):
+        if self.get_batch:
+            img_batch   = torch.zeros( ( self.batch_size, 1, self.cropsize, self.cropsize ), dtype=torch.float )
+            entry_batch = torch.zeros( self.batch_size, dtype=torch.long )
+            num_nonzero_batch = torch.zeros( self.batch_size, dtype=torch.float )
+
+            self.randomize_offset()
+            
+            for ib in range(self.batch_size):
+                img,entry,nonzero = self.get_one_cropped_image(ib)
+                img_batch[ib] = img
+                entry_batch[ib] = entry
+                num_nonzero_batch[ib] = nonzero
+
+            return img_batch,entry_batch,num_nonzero_batch
+        else:
+            return self.get_one_cropped_image(idx)
+
+    def get_one_cropped_image(self, idx):
+        #worker_info = torch.utils.data.get_worker_info()        
+        #print("relarmaeDataset[",self,"].get[",idx,"]. workerid=",worker_info.id)
+        #
+        # we expect this function to run inside a forked process function
+        # see relarmae_mp_dataloader.py
+                
+        # we use ROOT to get us an image.
+        # we then produce a random crop.
+        #  this crop needs to have enough patches that are nonzero
+        #  so we loop up to 10 tries to get such a crop.
+        #  if we fail, we move onto the next entry in the ROOT tree
+
+        # avoid referencing dictionaries and lists
+        # -- memory leak issue due to reference counts when multi-processing?
+        # https://github.com/pytorch/pytorch/issues/13246
+                
+        # what we return, as part of a tuple
+        img = None
+        entry = None
+        num_non_zero = None
+
+        okentry = False # indicate if we have an entry that returned a good crop
+        num_tries = 0 # number of entries we've looped through to find a good crop to return
+        ioffset = int(self.offset[0]) # entry number offset we'll reference for sequentially filling a batch
+
+        # loop over entries to find a good crop
+        while not okentry:
+            okentry = False
+            entry = idx+ioffset # entry we'll read
+            if entry>=self.nentries:
+                # reset the offset to work at the beginning of the file
+                entry = 0
+                self.offset[0] = -idx
+                ioffset = -idx
+                
+            # read data from file
+            self.tree.GetEntry(entry)
+
+            # get numpy array containing image
+            img = np.copy(self.tree.img_v.at( int(self.vector_index) ).tonumpy_nocopy())
+
+            # now we need to find a crop
+            cropok = False  # have we found a good crop?
+            croptries = 0   # number of tries for the current entry
+            while croptries<10 and not cropok:
+                croptries += 1
+                # define crop boundary
+                bounds_xmax = img.shape[0]-self.cropsize-1
+                bounds_ymax = img.shape[1]-self.cropsize-1
+                x = self.rng.randint(0,bounds_xmax)
+                y = self.rng.randint(0,bounds_ymax)
+                # make the crop
+                crop = img[x:x+self.cropsize,y:y+self.cropsize]
+
+                # now we need to decide if crop is good.
+                # we chunk the image into the patches the ViT will see.
+                # we need a certain number of patches to have a minimum number of pixels
+                # and a minimum number of patches with nonzero sums
+                if (crop[crop>self.adc_threshold]).sum()>self.min_crop_pixels:
+                    #data["img"] = np.expand_dims( crop, axis=0 ) # change to (1,H,W)
+                    cropresize = np.expand_dims( crop, axis=0 ) # change to (1,H,W)
+
+                    tensor = torch.from_numpy( np.expand_dims(cropresize,axis=0) ) # expand to (1,1,H,W)
+
+                    # calc nonzero patches: determines if we return crop
+                    with torch.no_grad():
+                        #print("tensor: ",tensor.shape)
+                        patches = self._chunk( tensor ) # returns (1,num_patches,patchdim1*patchdim2)
+                        #print("patches: ",patches)
+                        patchsum = torch.sum(patches,2).squeeze() # (num_patches)
+                        #print("patchsum: ",patchsum)
+                        num_non_zero = (patchsum>10).sum().item() # float
+                        #print(num_non_zero)
+                        #data["num_nonzero_patches"] = num_non_zero
+                        if num_non_zero >= self.nonzero_patch_threshold:
+                            cropok = True
+                            okentry = True
+
+                            # it's good, so make final scaled image
+                            #data["img"] = np.clip( (data["img"]-20.0)/50.0, -1.0, 5.0 )
+                            del img
+                            img = torch.from_numpy( np.clip( (cropresize-20.0)/50.0, -1.0, 5.0 ) )
+
+            if not okentry:
+                # will need to try again
+                ioffset += 1
+                self.offset[0] += 1
+            num_tries += 1
+
+        return img,entry,num_non_zero
+
+
+    def __len__(self):
+        return self.nentries
+
+    def _chunk(self,x):
+        #print(x.shape)
+        layer = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = self.patch_dims, p2 = self.patch_dims)
+        x = layer(x)
+        #print(x.shape)        
+        return x
+
+    def chunk(x,patch_dims):
+        layer = Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)',p1=patch_dims, p2=patch_dims)
+        x = layer(x)
+        return x
+    
+    def collate_fn(batch):
+        x = torch.utils.data.dataloader.default_collate(batch)
+        # for user ease
+        return {"img":x[0],"entry":x[1],"num_nonzero_patches":x[2]}
+
+    def collate_fn_batch(batch):
+        return {"img":batch[0][0],"entry":batch[0][1],"num_nonzero_patches":batch[0][2]}
+        
+    
+if __name__ == "__main__":
+    
+    import time
+    from larcv import larcv
+    larcv.load_pyutil()
+
+    from psutil import Process    
+
+    cfg = """\
+larmaeDataset:
+  filelist:
+#    - /cluster/tufts/wongjiradlabnu/twongj01/larmae/dataprep/data/mcc9_v29e_dl_run3_G1_extbnb_dlana/larmaedata_run3extbnb_0000.root
+#    - /n/home01/twongjirad/mcc9_v29e_dl_run3_G1_extbnb_dlana/train/larmaedata_run3extbnb_0000.root
+#    - /n/home01/twongjirad/mcc9_v29e_dl_run3_G1_extbnb_dlana/train/larmaedata_run3extbnb_*.root
+    - larmaedata_run3extbnb_0000.root
+  crop_size: 512
+  adc_threshold: 10.0
+  min_crop_pixels: 1000
+  vector_index: 0
+  batch_size: 64
+  return_batch: True
+"""
+
+    with open('tmp.yaml','w') as f:
+        print(cfg,file=f)
+    
+    niter = 20
+    test = relarmaeDataset( 'tmp.yaml' )
+    print("NENTRIES: ",len(test))
+    shuffle = False
+    FAKE_NET_RUNTIME = 0.250
+    
+    loader = torch.utils.data.DataLoader(test,batch_size=1,
+                                         shuffle=shuffle,
+                                         prefetch_factor=2,
+                                         #num_workers=2,
+                                         collate_fn=relarmaeDataset.collate_fn_batch)
+
+    dt_load = 0.0    
+    start = time.time()
+    for iiter in range(niter):
+
+        print("====================================")
+        print("ITER[%d]"%(iiter))
+
+        dt_iter = time.time()
+        batch = next(iter(loader))
+        dt_iter = time.time()-dt_iter
+        dt_load += dt_iter
+
+        if FAKE_NET_RUNTIME>0:
+            print("pretend network: lag=",FAKE_NET_RUNTIME)
+            time.sleep( FAKE_NET_RUNTIME )        
+        
+        print(" keys: ",batch.keys())
+        for name,d in batch.items():
+            if type(d) is torch.Tensor:
+                print("  ",name,"-[array]: ",d.shape)
+            elif type(d) is list:
+                print("  ",name,"-list of n=",len(d))
+            else:
+                print("  ",name,"-[non-array]: ",type(d))
+
+        # test the patch making routines
+        #x = test.chunk( batch['img'] )
+        #y = torch.sum(x,2)
+        #for b in range(y.shape[0]):
+        #    print(" batch[",b,"]: non-zero patches = ",(y[b,:]>10).sum())
+        #print(y.shape)
+        print(batch["num_nonzero_patches"])
+        print(batch["entry"])
+        print("main process: %.03f"%(Process().memory_info().rss/1.0e9)," GB")
+        
+    end = time.time()
+    elapsed = end-start
+    sec_per_iter = elapsed/float(niter)
+
+    print("loading time per iteration: ",dt_load/float(niter))    
+    print("WALL sec per iter: ",sec_per_iter)
+
+
+    
---
+++ relarmae_mp_dataloader.py
@@ -0,0 +1,265 @@
+import os,time,copy,sys
+import torch
+#import multiprocessing
+import torch.multiprocessing as mp
+import queue
+import itertools
+from itertools import cycle
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from relarmae_dataset import relarmaeDataset
+import gc
+
+from psutil import Process
+from mem_utils import MemoryMonitor
+
+def worker_fn(dataset, index_queue, output_queue, worker_idx ):
+    
+    # this is so stupid
+    current_buffer = -1
+    worker_buffer_0 = None
+    worker_buffer_1 = None
+    worker_buffer_2 = None
+    
+    while True:
+        # Worker function loop, simply reads indices from index_queue, and adds the
+        # dataset element to the output_queue
+        
+        try:
+            # check for request for data
+            #print("worker[",worker_idx,"] check request queue")
+            index = index_queue.get(timeout=0.01)
+            #index = index_queue.get()
+
+            if index is None:
+                print("worker[%d] saw index=None. Stop worker."%(worker_idx))
+                #sys.stdout.flush()
+                break
+            
+            if current_buffer>=0:
+                print("worker[",worker_idx,"] index[",index,"] using internal queue. buffer=",current_buffer)
+                if current_buffer==0:
+                    x = worker_buffer_0
+                elif current_buffer==1:
+                    x = worker_buffer_1
+                elif current_buffer==2:
+                    x = worker_buffer_2
+            else:
+                print("worker[",worker_idx,"] internal queue is empty, get data from loader, idx=",index)
+                x = dataset[0]
+
+            #print("worker[%d] queueing index="%(worker_idx),index," with loader=",loader[worker_idx])                
+            output_queue.put((index,x))
+
+            if current_buffer>=0:
+                # replace buffer with None and deref last ref x
+                if current_buffer==0:
+                    worker_buffer_0 = None
+                elif current_buffer==1:
+                    worker_buffer_1 = None
+                elif current_buffer==2:
+                    worker_buffer_2 = None
+                del x
+                current_buffer -= 1
+
+            gc.collect()
+            print("worker process mem usage: %0.3f GB"%(Process().memory_info().rss/1.0e9))
+            #print("gc: count=",gc.get_count())
+            #sys.stdout.flush()
+            continue
+            
+        except queue.Empty:
+            #print("no request. fill worker queue")
+            #if len(worker_index_queue)<batch_size:
+            if current_buffer<2:
+                #x = next(iter(loader[worker_idx]))
+                current_buffer += 1
+                if current_buffer==0:
+                    worker_buffer_0 = dataset[0]
+                elif current_buffer==1:
+                    worker_buffer_1 = dataset[0]
+                elif current_buffer==2:
+                    worker_buffer_2 = dataset[0]
+
+                print("fill worker[",worker_idx,"] buffer idx=",current_buffer)
+                print("worker process mem usage: %0.3f GB"%(Process().memory_info().rss/1.0e9))
+                #print("gc: count=",gc.get_count())                
+                #sys.stdout.flush()
+            #print("worker queue len=",len(worker_index_queue))
+
+            continue
+
+
+class relarmaeMultiProcessDataloader():
+    def __init__(self, data_loader_config, rank,
+                 num_workers=2,
+                 prefetch_batches=2):
+
+        self.index = 0
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.prefetch_batches = prefetch_batches
+        self.output_queue = mp.Queue()
+        self.index_queues = []
+        self.workers = []
+        self.worker_cycle = cycle(range(num_workers))
+        self.cache = {}
+        self.prefetch_index = 0
+        self.nentries = 0
+        self.shuffle = True
+        self.data_keys = None
+        self.datasets = []
+
+        self.data_loader_config = data_loader_config
+        self.pids = []
+        self.pids.append(os.getpid())
+        
+        for iworker in range(num_workers):
+            self.datasets.append( relarmaeDataset( data_loader_config, seed=num_workers*rank+iworker ) )
+            index_queue = mp.Queue()            
+            self.index_queues.append(index_queue)
+                
+        for iworker in range(num_workers):
+            worker = mp.Process(
+                target=worker_fn, args=(self.datasets[iworker], self.index_queues[iworker], self.output_queue, iworker)
+            )
+            worker.daemon = True
+            worker.start()
+            self.pids.append(worker.pid)            
+            self.workers.append(worker)
+            
+        #mp.set_forkserver_preload(["torch"])
+        #ctx = torch.multiprocessing.start_processes(
+        #    worker_fn,
+        #    (self.datasets[iworker], self.index_queues[iworker], self.output_queue, iworker),
+        #    nprocs=num_workers, join=False, daemon=True,
+        #    start_method='fork')
+        self.monitor = MemoryMonitor(self.pids)
+
+        self.prefetch()
+        print("prefetch finished")
+
+    def prefetch(self):
+        print("prefetch")
+        #while (self.prefetch_index < self.batch_size):
+        #    # if the prefetch_index hasn't reached the end of the dataset
+        #    # and it is not 2 batches ahead, add indexes to the index queues
+        #    self.index_queues[next(self.worker_cycle)].put(self.prefetch_index)
+        #    self.prefetch_index += 1
+        self.index_queues[next(self.worker_cycle)].put(0)
+
+    def __iter__(self):
+        self.index = 0
+        self.cache = {}
+        self.prefetch_index = 0
+        #self.prefetch()
+        return self
+
+    def __next__(self):
+        #if self.index >= self.nentries:
+        #    raise StopIteration
+        print("next")
+        #out = [self.get() for _ in range(self.batch_size)]
+        #out = [self.get() for _ in range(1)]
+        #return self.collate_fn(out)
+        x = self.get()
+        #print("NEXT: ",out)
+        #return relarmaeDataset.collate_fn_batch(out)
+        self.prefetch()
+        return {"img":x[0],"entry":x[1],"num_nonzero_patches":x[2]}
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def get(self):
+        #print("start prefetch")
+        #self.prefetch()
+        #print("check cache")        
+        if self.index in self.cache:
+            item = self.cache[self.index]
+            del self.cache[self.index]
+        else:
+            #print("check queue")            
+            while True:
+                try:
+                    (index, data) = self.output_queue.get()
+                except queue.Empty:  # output queue empty, keep trying
+                    continue
+                if index == self.index:  # found our item, ready to return
+                    item = data
+                    break
+                else:  # item isn't the one we want, cache for later
+                    self.cache[index] = data
+        #print("increment index")            
+        self.index += 1
+        return item
+
+    def __del__(self):
+        try:
+            for i, w in enumerate(self.workers):
+                self.index_queues[i].put(None)
+                w.join(timeout=1000.0)
+            for q in self.index_queues:
+                q.cancel_join_thread()
+                q.close()
+            self.output_queue.cancel_join_thread()
+            self.output_queue.close()
+        finally:
+            for w in self.workers:
+                if w.is_alive():
+                    w.terminate()
+        
+if __name__ == "__main__":
+    
+    import yaml
+    from larcv import larcv
+    larcv.load_pyutil()
+    
+    config = "config_train.yaml"
+    FAKE_NET_RUNTIME = 0.250
+    niters = 20000000
+    batch_size = 64
+    num_workers = 2
+    rank = 0
+    loader = relarmaeMultiProcessDataloader(config, rank,
+                                            num_workers=num_workers,
+                                            prefetch_batches=1)
+
+    print("START LOOP")
+    print("[enter] to continue")
+    input()
+
+    start = time.time()
+    dt_load = 0.0
+    for iiter in range(niters):
+        print("-------------------")
+        print("ITER ",iiter)
+        dt_iter = time.time()
+        batch = next(iter(loader))
+        #print(batch.keys())
+        dt_iter = time.time()-dt_iter
+        dt_load += dt_iter
+
+        print("main process mem usage: %0.3f GB"%(Process().memory_info().rss/1.0e9))        
+        if FAKE_NET_RUNTIME>0:
+            print("pretend network: lag=",FAKE_NET_RUNTIME)
+            time.sleep( FAKE_NET_RUNTIME )
+        
+        # Dump data for debug
+        print("ENTRY: ",batch["entry"])
+        #print("ENTRY: ",batch[1])
+        if False:
+            print("batch: ",batch)
+            print(batch["entry"])
+            for ib in range(batch["img"].shape[0]):
+                print("img[",ib,"] check: ",batch["img"][ib,:].sum())
+                print("nonzero dump: ")
+                x = batch["img"][ib].reshape(-1)
+                print(x[x>-0.4][:10])
+        print(loader.monitor.table())
+                
+    print("MADE IT")
+    print("loading time per iteration: ",dt_load/float(niters))
+    end = time.time()
+    print("WALL time per iter: ",(end-start)/float(niters))
